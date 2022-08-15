68288c15bb7b271c0b8b2e808f9db18f02a4daa9
Austin Liu <austinhliu@gmail.com>
2022-08-15 15:54:26 -0700

    Disable illuminate by default on neovim

--- neovim/.config/nvim/lua/austin/config/illuminate.lua
+++ neovim/.config/nvim/lua/austin/config/illuminate.lua
@@ -1,5 +1,5 @@
 vim.g.Illuminate_delay = 0
 vim.g.Illuminate_ftblacklist = { '' }
 
--- vim.fn['illuminate#disable_illumination'](0)
+vim.fn['illuminate#disable_illumination'](0)
 vim.keymap.set("n", "<leader>til", "<cmd>IlluminationToggle<CR>", {noremap = true, silent = true})
