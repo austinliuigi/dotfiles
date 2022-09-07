71efa325541d78bb92cf139f803e3b51dc1bc16a
Austin Liu <austinhliu@gmail.com>
2022-09-07 04:28:32 -0700

    Neovim: Add cmp cmdline history and refactor config

--- nvim/.config/nvim/lua/austin/config/cmp.lua
+++ nvim/.config/nvim/lua/austin/config/cmp.lua
@@ -1,25 +1,33 @@
-local cmp = require 'cmp'
+local cmp = require('cmp')
 
 cmp.setup {
+  enabled = function()
+    return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt"
+        or require("cmp_dap").is_dap_buffer()
+  end,
   completion = {
     completeopt = 'menu,menuone,noinsert'
   },
   mapping = cmp.mapping.preset.insert({
-    ['<Left>'] = cmp.mapping(cmp.mapping.close(), { 'i', 'c' }),
-    ['<Right>'] = cmp.mapping(cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Insert }), { 'i', 'c' }),
-    ['<Up>'] = cmp.mapping(cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }), { 'i', 'c' }),
-    ['<Down>'] = cmp.mapping(cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }), { 'i', 'c' }),
-    ['<S-Up>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-    ['<S-Down>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+    ['<S-Left>'] = cmp.mapping(cmp.mapping.close(), { 'i', 'c' }),
+    ['<S-Right>'] = cmp.mapping(cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Insert }), { 'i', 'c' }),
+    -- ['<Esc>'] = cmp.mapping(cmp.mapping.close(), { 'i', 'c' }),
+    -- ['<CR>'] = cmp.mapping(cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Insert }), { 'i', 'c' }),
+    ['<S-Up>'] = cmp.mapping(cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }), { 'i', 'c' }),
+    ['<S-Down>'] = cmp.mapping(cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }), { 'i', 'c' }),
+    ['<C-Up>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+    ['<C-Down>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
     ['<S-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
   }),
   sources = {
-    { name = 'nvim_lsp_signature_help' },
-    { name = 'nvim_lua' },
-    { name = 'nvim_lsp' },
-    { name = 'path' },
     { name = 'luasnip' },
-    { name = 'buffer', keyword_length = 3 }
+    { name = 'nvim_lsp_signature_help' },
+    { name = 'nvim_lsp' },
+    { name = 'nvim_lua' },
+    { name = 'path' },
+    { name = 'calc' },
+    { name = 'buffer', keyword_length = 2, max_item_count = 4},
+    -- { name = 'digraphs' },
   },
   snippet = {
     expand = function(args)
@@ -59,24 +67,37 @@ cmp.setup {
       vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
       -- vim_item.kind = string.format("%s %s", kind_icons[vim_item.kind], vim_item.kind)
       vim_item.menu = ({
-        buffer = '[BUF]',
-        path = '[PATH]',
-        cmdline = '[CMD]',
-        nvim_lua = '[API]',
-        nvim_lsp = '[LSP]',
+        luasnip = '[SNIP]',
         nvim_lsp_signature_help = '[SIG]',
-        luasnip = '[SNIP]'
+        nvim_lsp = '[LSP]',
+        nvim_lua = '[API]',
+        path = '[PATH]',
+        calc = '[CALC]',
+        buffer = '[BUF]',
+        dap = '[DAP]',
+        cmdline_history = '[HIST]',
+        cmdline = '[CMD]',
       })[entry.source.name]
       return vim_item
     end
   },
   window = {
-    documentation = cmp.config.window.bordered()
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
   },
   experimental = {
     ghost_text = true
   }
 }
+
+cmp.setup.filetype({ "dap-repl", "dapui_watches" }, {
+  sources = {
+    { name = "dap", trigger_characters = { "." }  },
+  },
+  formatting = {
+    fields = { 'kind', 'abbr', 'menu' },
+  },
+})
 
 cmp.setup.cmdline(':', {
   -- completion = {
@@ -84,7 +105,8 @@ cmp.setup.cmdline(':', {
   -- },
   sources = {
     { name = 'path' },
-    { name = 'cmdline', max_item_count = 10 }
+    { name = 'cmdline', max_item_count = 5 },
+    { name = 'cmdline_history', max_item_count = 2, keyword_length = 5 },
   },
   formatting = {
     fields = { 'kind', 'abbr', 'menu' },
@@ -94,26 +116,22 @@ cmp.setup.cmdline(':', {
   }
 })
 
-cmp.setup.cmdline('/', {
-  -- completion = {
-  --   keyword_length = 2
-  -- },
-  sources = {
-    { name = 'buffer', max_item_count = 10 }
-  },
-  formatting = {
-    fields = { 'kind', 'abbr', 'menu' },
-  },
-  window = {
-    completion = cmp.config.window.bordered()
-  }
-})
+for _, cmd_type in ipairs({'/', '?'}) do
+  cmp.setup.cmdline(cmd_type, {
+    -- completion = {
+    --   keyword_length = 2
+    -- },
+    sources = {
+      { name = 'buffer', max_item_count = 5 },
+      { name = 'cmdline_history', max_item_count = 2, keyword_length = 5 },
+    },
+    formatting = {
+      fields = { 'kind', 'abbr', 'menu' },
+    },
+    window = {
+      completion = cmp.config.window.bordered()
+    }
+  })
+end
 
---vim.cmd([[
---  hi! link CmpItemAbbrMatch Normal
---  hi! link CmpItemAbbrMatchFuzzy CmpItemAbbrMatch
---  hi! link CmpItemAbbr Comment
---  hi! link CmpItemAbbrDeprecated Error
---  hi! link CmpItemKind Special
---  hi! link CmpItemMenu NonText
---]])
+vim.api.nvim_exec_autocmds("User", { pattern = "CmpConfigLoaded" })
