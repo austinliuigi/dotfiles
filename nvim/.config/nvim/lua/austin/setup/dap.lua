0e0a7d43daaca30f7eb0065cf6cce40a3d3166ba
Austin Liu <austinhliu@gmail.com>
2022-09-11 23:13:28 -0700

    Neovim: Add setup files for packer

---
+++ nvim/.config/nvim/lua/austin/setup/dap.lua
@@ -0,0 +1,15 @@
+local map = vim.keymap.set
+
+map('n', "<Left>",  function() return require("dap").step_back() end, {})
+map('n', "<Down>",  function() return require("dap").step_out() end,  {})
+map('n', "<Up>",    function() return require("dap").step_into() end, {})
+map('n', "<Right>", function() return require("dap").step_over() end, {})
+
+map('n', "<S-Left>",  function() return require("dap").reverse_continue() end, {})
+map('n', "<S-Down>",  function() return require("dap").terminate() end,  {})
+map('n', "<S-Up>",    function() return require("dap").repl.toggle() end, {})
+map('n', "<S-Right>", function() return require("dap").continue() end, {})
+
+map('n', "<leader>tb", function() return require("dap").toggle_breakpoint() end, {})
+map('n', "<leader>tB", function() return require("dap").set_breakpoint(vim.fn.input("Breakpoint Condition: ")) end, {})
+-- map('n', "<leader>tB", function() return vim.ui.input({ prompt = 'Breakpoint Condition: ' }, function(input) return require('dap').toggle_breakpoint(input) end) end, {})
---
+++ nvim/.config/nvim/lua/austin/setup/nvim-tree.lua
@@ -0,0 +1,11 @@
+vim.api.nvim_create_augroup("NvimTreeLazyLoad", {clear = true})
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  group   = "NvimTreeLazyLoad",
+  pattern = "*",
+  callback = function()
+    if vim.fn.isdirectory(vim.fn.expand('%')) == 1 then
+      vim.cmd("NvimTreeToggle %")
+      vim.api.nvim_clear_autocmds({group = "NvimTreeLazyLoad"})
+    end
+  end
+})
