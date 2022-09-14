40c74c7c28f0beafe74bcb0fae773714867c563b
Austin Liu <austinhliu@gmail.com>
2022-09-14 16:41:02 -0700

    Neovim: Add <S-CR> and <C-CR> breakpoint mappings

--- nvim/.config/nvim/lua/austin/setup/dap.lua
+++ nvim/.config/nvim/lua/austin/setup/dap.lua
@@ -10,6 +10,8 @@ map('n', "<S-Down>",  function() return require("dap").terminate() end,  {})
 map('n', "<S-Up>",    function() return require("dap").repl.toggle() end, {})
 map('n', "<S-Right>", function() return require("dap").continue() end, {})
 
+map('n', "<S-CR>", function() return require("dap").toggle_breakpoint() end, {})
+map('n', "<C-CR>", function() return require("dap").set_breakpoint(vim.fn.input("Breakpoint Condition: ")) end, {})
 map('n', "<leader>tb", function() return require("dap").toggle_breakpoint() end, {})
 map('n', "<leader>tB", function() return require("dap").set_breakpoint(vim.fn.input("Breakpoint Condition: ")) end, {})
 -- map('n', "<leader>tB", function() return vim.ui.input({ prompt = 'Breakpoint Condition: ' }, function(input) return require('dap').toggle_breakpoint(input) end) end, {})
