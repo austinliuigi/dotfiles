f7f5ab56ff4f09076c1be1eede9b3c81301a4a3c
Austin Liu <austinhliu@gmail.com>
2022-12-21 21:33:31 -0800

    Neovim: Add check for kitty terminal in noice config

--- nvim/.config/nvim/lua/austin/config/noice.lua
+++ nvim/.config/nvim/lua/austin/config/noice.lua
@@ -195,7 +195,7 @@ require("noice").setup {
   format = {}, --- @see section on formatting
 }
 
-local term_bg = vim.fn.system("awk '$1 ~ /^background$/ {print $2}' ~/.config/kitty/current-theme.conf")
+local term_bg = os.getenv('TERM') == 'xterm-kitty' and vim.fn.system("awk '$1 ~ /^background$/ {print $2}' ~/.config/kitty/current-theme.conf") or "#000000"
 require("notify").setup {
   background_colour = term_bg
 }
