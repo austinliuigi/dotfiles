41f895742492f8afd382c63636b9e2a6e9f776ae
Austin Liu <austinhliu@gmail.com>
2022-12-21 20:49:41 -0800

    Neovim: Update treesitter config

--- nvim/.config/nvim/lua/austin/config/treesitter.lua
+++ nvim/.config/nvim/lua/austin/config/treesitter.lua
@@ -2,6 +2,8 @@ local treesitter = require('nvim-treesitter.configs')
 
 treesitter.setup {
   auto_install = true,
+  ensure_installed = "all",
+  sync_install = true,
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = false,
